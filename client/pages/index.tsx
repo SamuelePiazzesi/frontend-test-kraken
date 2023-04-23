bdc5c968e67fd9ad3991bb728c4329c5f439f412
Samuele Piazzesi <samuelepiazzesi@gmail.com>
2023-04-23 18:42:59 +0200

    chore: minor home changes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,19 +1,23 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <main>
-      <div className="home">
-        <figure>
-          <img
-            src="https://static.octopuscdn.com/logos/logo.svg"
-            alt="Octopus Energy Logo"
-          />
-        </figure>
-        <h1>Welcome to the Octopus Energy Frontend code test!</h1>
-        <p>
-          Get started by visiting the <code>/product</code> URL and editing{" "}
-          <code>client/pages/product.js</code>
-        </p>
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className="flex flex-col justify-center items-center h-screen">
+				<figure>
+					<img
+						src="https://static.octopuscdn.com/logos/logo.svg"
+						alt="Octopus Energy Logo"
+					/>
+				</figure>
+				<h1>Welcome to the Octopus Energy Frontend code test!</h1>
+
+				<Link href="/product/1">
+					<p className="p-4 cursor-pointer bg-soholights mt-4 rounded-lg">
+						Get started by visiting the product page
+					</p>
+				</Link>
+			</div>
+		</main>
+	);
 }
